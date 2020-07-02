c89dcb4069f22332bd9fbdfb17425370fb064271
sayali1098 <sauamritkar@gmail.com>
2020-07-02 10:50:18 +0530

    FSD0620B102

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
@@ -11,16 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.User;
+import  businesslogic.RegistrationValidation;
 
 
 @WebServlet(urlPatterns= {"/signup"})
 public class SignUpController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
 
     public SignUpController() {
-        super();
-       
+        super();    
     }
 
 
@@ -31,34 +31,38 @@ public class SignUpController extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	/*	String email = request.getParameter("email"); 
+		
+		
+		String email = request.getParameter("email"); 
 		String password = request.getParameter("password"); 
 		String confirmPassword = request.getParameter("confirmPassword"); 
 		LocalDateTime date= LocalDateTime.now(); 
 		
 
+		RegistrationValidation checkUser = new RegistrationValidation();
+		checkUser.checkUserDetails(email, password, confirmPassword);
 		
-		if(checkUser.checkUserDetails(email, password, confirmPassword))
-		{
-			User user=new User(email,password,date);
-			user.setEmail(email);
-			user.setPassword(password);
-			user.setDate(date);
-			System.out.println(user.getEmail());
-			System.out.println(user.getPassword());
-			System.out.println(user.getDate());
-			request.setAttribute("message", "Registration Successful");
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
-			rd.forward(request, response);
-		}
-		else
-		{
-			request.setAttribute("message", "Check your email and password");
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
-			rd.forward(request, response);
-		}
+//		if(checkUser.checkUserDetails(email, password, confirmPassword))
+//		{
+//			User user=new User(email,password,date);
+//			user.setEmail(email);
+//			user.setPassword(password);
+//			user.setDate(date);
+//			System.out.println(user.getEmail());
+//			System.out.println(user.getPassword());
+//			System.out.println(user.getDate());
+//			request.setAttribute("message", "Registration Successful");
+//			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
+//			rd.forward(request, response);
+//		}
+//		else
+//		{
+//			request.setAttribute("message", "Check your email and password");
+//			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
+//			rd.forward(request, response);
+//		}
+//		
 		
-		*/
 	}
 
 }
